7b37a9b9c8b9824694a2f0f6eedc0a7c8dfe207d
Taylor Callsen <gritto2008@gmail.com>
2022-01-30 11:37:19 -0800

    tweaking actualtor and logging output settings

--- src/main/java/me/callsen/taylor/romannumerals/config/Config.java
+++ src/main/java/me/callsen/taylor/romannumerals/config/Config.java
@@ -1,11 +1,18 @@
 package me.callsen.taylor.romannumerals.config;
 
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @Configuration
 public class Config {
+
+  @Bean
+  public HttpTraceRepository httpTraceRepository() {
+      return new InMemoryHttpTraceRepository();
+  }
 
   @Bean
   public MethodValidationPostProcessor methodValidationPostProcessor() {
